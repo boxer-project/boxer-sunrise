f14e764584e2fa2c6137f29b1b41db7e41054db6
Steven Githens <steve@githens.org>
2019-11-24 14:35:34 -0800

    Initial import of disdcl.lisp from existing boxer code

---
+++ src/disdcl.lisp
@@ -0,0 +1,242 @@
+; -*- Mode:LISP; Syntax: Common-Lisp; Package:BOXER;-*- 
+#|
+
+
+ $Header: disdcl.lisp,v 1.0 90/01/24 22:09:27 boxer Exp $
+
+
+ $Log:	disdcl.lisp,v $
+;;;Revision 1.0  90/01/24  22:09:27  boxer
+;;;Initial revision
+;;;
+
+
+
+
+
+
+
+
+ Copyright 1986 - 1998 Regents of the University of California
+
+ Enhancements and Modifications Copyright 1999 - 2012 Pyxisystems LLC
+
+                                         +-Data--+
+                This file is part of the | BOXER | system
+                                         +-------+
+
+  This file contains only the variable declarations and Macro definitions that
+  need to loaded in BEFORE all of the other files in the DISPLAYDEFS module.
+
+
+
+Modification History (most recent at the top)
+
+ 1/21/12 changed the definition of sprite-screen-box class
+ 9/18/10 show-redisplay-init-code
+ 6/04/09 support for redisplaying changed horizontal scrolling in redisplaying-unit 
+ 2/10/09 removed inf-shift, cached-border-info slots from SB's and out-of-sync-mark slot from SR's
+ 2/07/09 removed new-{wid,hei} and new-{x,y}-got-clipped? slots from screen-obj
+         added max-scroll-wid for screen-boxes
+ added sprite-screen-box class  for sprites representation in redisplay
+ 2/01/05 added *redisplay-id*, *redisplay-in-progress?*, redisplaying-unit &
+         related accessors
+ 6/18/04 change %origin-y-offset initial value for #+mcl to be 0 (from 15)
+         because it was breaking mouse doc tracking.  Keep and eye out for
+         possible future problems though %origin-y-offset is usually accessed
+         in a rebound environment
+ 2/11/03 merge LW and MCL source, updated copyright
+ 4/28/98 Started Logging changes: source = boxer version 2.2.r4
+
+|#
+
+(in-package :boxer)
+
+;;;; Objects Having To Do With Redisplay.
+
+
+(defvar *screen-chas-array-default-size* 32.)
+
+(defstruct (screen-chas-array (:type vector)
+			      (:include storage-vector)
+			      (:constructor %make-screen-chas-array)
+			      (:copier nil) ;; we do this ourselves
+			      )
+  (fds nil))
+
+(defun make-screen-chas-array (&optional
+			       (length *screen-chas-array-default-size*))
+  (%make-screen-chas-array :contents (make-array length)))
+
+;; also see the ACTUAL-OBJ-SUBCLASS which is neccessary
+;; for Editor objects to be displayed
+;;; OpenGL Note: can probably drop half of these slots
+;;; in particular, new-??, needs-redisplay-pass-2? & force-redisplay-infs?
+;;; need to think more about whether the clipped? stuff is neccessary
+
+(defclass SCREEN-OBJ
+    ()
+  ((actual-obj :initform nil :accessor screen-obj-actual-obj)
+   (x-offset :initform 0 :accessor screen-obj-x-offset)
+   (y-offset :initform 0 :accessor screen-obj-y-offset)
+   (wid :initform 0 :accessor screen-obj-wid)
+   (hei :initform 0 :accessor screen-obj-hei)
+   (x-got-clipped? :initform nil :accessor screen-obj-x-got-clipped?)
+   (y-got-clipped? :initform nil :accessor screen-obj-y-got-clipped?)
+;   (new-wid :initform 0 :accessor screen-obj-new-wid)
+;   (new-hei :initform 0 :accessor screen-obj-new-hei)
+;   (new-x-got-clipped? :initform nil :accessor screen-obj-new-x-got-clipped?)
+;   (new-y-got-clipped? :initform nil :accessor screen-obj-new-y-got-clipped?)
+   (tick :initform -1 :accessor screen-obj-tick)
+   (needs-redisplay-pass-2? :initform nil :accessor screen-obj-needs-redisplay-pass-2?)
+   (force-redisplay-infs? :initform nil :accessor screen-obj-force-redisplay-infs?))
+  (:metaclass block-compile-class)
+  (:abstract-class t))
+
+;; These only exist as a mixin for the box flavor
+(defclass SCREEN-CHAR-SUBCLASS
+    (screen-obj)
+  ((screen-row :initform nil :accessor screen-row))
+  (:metaclass block-compile-class))
+
+(defclass SCREEN-ROW
+    (screen-obj)
+  ((screen-box :initform nil :accessor screen-box)
+   (screen-chas :initform (make-screen-chas-array) :accessor screen-chas)
+;   (out-of-synch-mark :initform nil :accessor out-of-synch-mark)
+   (baseline :initform 0 :accessor baseline))
+  (:metaclass block-compile-class))
+
+
+(defclass SCREEN-BOX
+    (screen-char-subclass)
+  ((screen-rows :initform (allocate-storage-vector 8.) :accessor screen-rows)
+   (scroll-to-actual-row :initform nil :accessor scroll-to-actual-row)
+   ;; how much to slosh inferiors when the borders change
+;   (inf-shift :initform nil :accessor inf-shift)  ; remove?
+   (name :initform nil :accessor name)
+   (box-type :initform ':doit-box :accessor box-type)
+   (bps :initform nil :accessor bps)
+   (display-style-list :initform (make-display-style :style nil)
+		       :accessor display-style-list)
+   (superior-screen-box :initform nil :accessor superior-screen-box)    
+;   (cached-border-info :initform nil :accessor cached-border-info)  ; remove?
+   (cached-absolute-pos :initform nil :accessor cached-absolute-pos)
+   ;; scrolling vars
+   (scroll-y-offset :initform 0)
+   (scroll-x-offset :initform 0)
+   (max-scroll-wid  :initform nil))
+  (:metaclass block-compile-class))
+
+
+(DEFUN CHECK-SCREEN-CHA-ARG (SCREEN-CHA)
+  (OR (CHARACTERP SCREEN-CHA)
+      (SCREEN-BOX? SCREEN-CHA)))
+
+(defclass GRAPHICS-SCREEN-BOX
+    (screen-box)
+  ()
+  (:metaclass block-compile-class))
+
+;;; this is for graphics display of sprite boxes when the containing
+;;; box is in text mode.  It has to inherit from screen-box so it can be toggled
+;;; to/from vanilla screen-box's
+(defclass sprite-screen-box
+   (screen-box)
+  ()
+  (:metaclass block-compile-class))
+
+
+
+;;;; Variable Declarations
+
+(defvar *redisplay-related-initializations* nil
+  "Forms on this list are run BEFORE the Redislay is
+   called usually after the window is created")
+
+(defvar *redisplay-initialization-list* nil
+  "A list of symbols to funcall.  The symbols are generated from
+   the forms in *redisplay-related-initializations*")
+
+(DEFVAR %DRAWING-WINDOW NIL
+  "Inside of a drawing-on-window, this variable is bound to the window which
+   was given as an argument to drawing-on window, makes sense right.")
+
+(DEFVAR %DRAWING-ARRAY NIL
+  "Inside of a drawing-on-window, this variable is bound to %drawing-window's
+   screen-array (Note that this value is valid because drawing-on-window does
+   a prepare-sheet of drawing-window.")
+
+(DEFVAR %DRAWING-FONT-MAP NIL
+  "Inside of a drawing-on-window, this variable is bound to %drawing-window's
+   font-map.")  
+
+(DEFVAR %ORIGIN-X-OFFSET 0
+  "Inside of a drawing-on-window, this variable is bound to x-offset of the
+   current drawing origin from the screen's actual x origin. With-origin-at
+   rebinds this variable (and %origin-y-offset) to change the screen position
+   of the drawing origin.")
+
+(DEFVAR %ORIGIN-Y-OFFSET 0
+  "Inside of a drawing-on-window, this variable is bound to y-offset of the
+   current drawing origin from the screen's actual y origin. With-origin-at
+   rebinds this variable (and %origin-x-offset) to change the screen position
+   of the drawing origin.")
+
+(DEFVAR %CLIP-LEF 0)
+(DEFVAR %CLIP-TOP 0)
+(DEFVAR %CLIP-RIG 0)
+(DEFVAR %CLIP-BOT 0)
+
+;;;; Font Vars....
+(defvar %drawing-font)
+
+(defvar %drawing-font-cha-wid)
+(defvar %drawing-font-cha-hei)
+
+(defvar %drawing-fit)
+(defvar %drawing-font-cha-wid-table)
+
+(defvar *redisplay-id* 0)
+(defvar *redisplay-in-progress?* nil)
+(defvar *redisplay-encore?* nil)
+(defvar *allow-redisplay-encore? nil)
+
+;; the innards of repaint-cursor can side effect the horizontal scrolling
+;; of the (point-screen-box).  If it does, then it will set this flag and then
+;; throw to 'scroll-x-changed TAG
+
+(defmacro redisplaying-unit (&body body)
+  `(let ((*redisplay-in-progress?* t)
+         (*redisplay-encore?* nil))
+     (catch 'scroll-x-changed
+       (unwind-protect
+           (progn . ,body)
+         (when (not (null *redisplay-encore?*))
+           (progn . ,body))
+         (setq *redisplay-id* (tick))))))
+
+(defun redisplay-in-progress? () *redisplay-in-progress?*)
+(defun redisplay-id () *redisplay-id*)
+
+
+
+;;;; Inits
+
+(defmacro def-redisplay-initialization (form)
+  (let ((name (intern (symbol-format nil "INIT-REDISPLAY-FUN-~A" (gensym)))))
+    `(progn
+       (defun ,name () ,form)
+       (unless (member ',form *redisplay-related-initializations* :test #'equal)
+	 (push ',form *redisplay-related-initializations*)
+	 (push ',name *redisplay-initialization-list*)))))
+
+
+(defun run-redisplay-inits ()
+  (dolist (initfun (reverse *redisplay-initialization-list*))
+    (funcall initfun)))
+
+
+(defun show-redisplay-init-code (fun)
+  (pp (nth (position fun *redisplay-initialization-list*)
+           *redisplay-related-initializations*)))
