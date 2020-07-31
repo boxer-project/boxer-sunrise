747ad709ed736047cd82869677efc2b6ce1b2521
Steven Githens <steve@githens.org>
2020-07-31 10:44:22 -0700

    Removing old start boxer method that checked for license registration. Minor in-progress hacks for running on windows.

--- src/bootstrap.lisp
+++ src/bootstrap.lisp
@@ -21,19 +21,25 @@
       (load #P"./src/bootstrap.lisp")
 
 |#
+(require "asdf")
 
+;; TODO fix this to preserve the windows logical drive
 (defvar *boxer-project-dir* (make-pathname :directory (butlast (pathname-directory *load-truename*))))
 
 (setf asdf:*central-registry*
                (list* '*default-pathname-defaults*
                       *boxer-project-dir*
+                      #+win32 #P"Z:/code/boxer-sunrise2/"
                 asdf:*central-registry*))
 
 (load (example-file "opengl/examples/load"))
 (setf *features* (cons :opengl *features*))
 (asdf:load-system :boxer-sunrise2)
 (setf boxer-window::*capogi-font-directory* (merge-pathnames "data/boxersunrise.app/Contents/Resources/Fonts/" *boxer-project-dir*))
-(load (merge-pathnames "run-tests.lisp" *boxer-project-dir*))
+
+#+win32(setf boxer-window::*capogi-font-directory* #P"Z:/code/boxer-sunrise2/data/boxersunrise.app/Contents/Resources/Fonts/")
+
+;; (load (merge-pathnames "run-tests.lisp" *boxer-project-dir*))
 (boxer-window::window-system-specific-make-boxer)
-(boxer-window::window-system-specific-start-boxer-1)
+(boxer-window::window-system-specific-start-boxer)
 (boxer-window::font-size-menu-action 3 nil)
