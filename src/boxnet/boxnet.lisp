4859969f535bb1afaba6e3d85a8748196580247d
Steven Githens <steve@githens.org>
2020-10-28 10:13:35 -0700

    Paredit indenting boxnet.lisp

--- src/boxnet/boxnet.lisp
+++ src/boxnet/boxnet.lisp
@@ -49,39 +49,39 @@ Modification History (most recent at top)
 
 #|
 (eval-when (load eval)
-  #+(and (not solaris) Lucid) (progn
-        #+sparc   (lcl:load-foreign-files
-               (merge-pathnames
-            (getf (sm::system-properties
-                   (find-system-named 'boxer))
-                  :pathname-default)
-            "receive.sun4.o"))
-        #+MC68000 (lcl:load-foreign-files
-               (merge-pathnames
-            (getf (sm::system-properties
-                   (find-system-named 'boxer))
-                  :pathname-default)
-            "receive.sun3.o"))
-        #+sparc   (lcl:load-foreign-files
-               (merge-pathnames
-            (getf (sm::system-properties
-                   (find-system-named 'boxer))
-                  :pathname-default)
-            "send.sun4.o"))
-        #+MC68000 (lcl:load-foreign-files
-               (merge-pathnames
-            (getf (sm::system-properties
-                   (find-system-named 'boxer))
-                  :pathname-default)
-            "send.sun3.o")))
-  )
+           #+(and (not solaris) Lucid) (progn
+                                        #+sparc   (lcl:load-foreign-files
+                                                   (merge-pathnames
+                                                    (getf (sm::system-properties
+                                                           (find-system-named 'boxer))
+                                                          :pathname-default)
+                                                    "receive.sun4.o"))
+                                        #+MC68000 (lcl:load-foreign-files
+                                                   (merge-pathnames
+                                                    (getf (sm::system-properties
+                                                           (find-system-named 'boxer))
+                                                          :pathname-default)
+                                                    "receive.sun3.o"))
+                                        #+sparc   (lcl:load-foreign-files
+                                                   (merge-pathnames
+                                                    (getf (sm::system-properties
+                                                           (find-system-named 'boxer))
+                                                          :pathname-default)
+                                                    "send.sun4.o"))
+                                        #+MC68000 (lcl:load-foreign-files
+                                                   (merge-pathnames
+                                                    (getf (sm::system-properties
+                                                           (find-system-named 'boxer))
+                                                          :pathname-default)
+                                                    "send.sun3.o")))
+           )
 
 ;;; At least remind during compilation....
 
 (eval-when (compile)
-  (format t "~%***~%NOTE:You may have to recompile the C files send.c ~%~
+           (format t "~%***~%NOTE:You may have to recompile the C files send.c ~%~
              and receive.c for this architecture~%***")
-  )
+           )
 
 |#
 
@@ -91,33 +91,33 @@ Modification History (most recent at top)
   #+MC68000                   "netsocket.sun3.o")
 
 (eval-when (eval load)
-  #+lucid (unless (null *foreign-netsocket-file*)
-        (lcl::load-foreign-files
-         (merge-pathnames
-          (getf (sm::system-properties (find-system-named 'boxer))
-            :pathname-default)
-          *foreign-netsocket-file*)))
-  )
+           #+lucid (unless (null *foreign-netsocket-file*)
+                     (lcl::load-foreign-files
+                      (merge-pathnames
+                       (getf (sm::system-properties (find-system-named 'boxer))
+                             :pathname-default)
+                       *foreign-netsocket-file*)))
+           )
 
 ;;;; compilation reminder (shouldn't the defsystem do this ?)
 (eval-when (compile)
-  (format t "~%***~%NOTE:You may have to recompile the C files netsocket.c ~%~
+           (format t "~%***~%NOTE:You may have to recompile the C files netsocket.c ~%~
              for this architecture~%***")
-  )
+           )
 
 
 ;;returns the file descriptor bits to give to select-and-accept.
 #+(and Lucid (not solaris))
 (lcl:define-c-function (socket-listen-and-bind "_socket_listen_and_bind")
-    ((port-number :integer))
-  :result-type :integer)
+                       ((port-number :integer))
+                       :result-type :integer)
 
 ;; returns a unix file handle for make-lisp-stream.
 #+(and Lucid (not solaris))
 (lcl:define-c-function (select-and-accept "_select_and_accept")
-    ((socket-object :integer)
-     (poll-time-in-usec :integer))
-  :result-type :integer)
+                       ((socket-object :integer)
+                        (poll-time-in-usec :integer))
+                       :result-type :integer)
 
 
 #+(and Lucid (not solaris))
@@ -126,8 +126,8 @@ Modification History (most recent at top)
 ;; returns a unix file handle for make-lisp-stream.
 #+(and Lucid (not solaris))
 (lcl:define-c-function (socket-open-and-connect "_socket_open_and_connect")
-    ((hostname :string) (port-number :integer))
-  :result-type :integer)
+                       ((hostname :string) (port-number :integer))
+                       :result-type :integer)
 
 
 
@@ -142,43 +142,43 @@ Modification History (most recent at top)
   (when (or (null *socket*) (not (plusp *socket*)))
     (let ((socket (socket-listen-and-bind *boxer-port-number*)))
       (when (minusp socket)
-    (warn "Failed to set up for remote connection:"))
+        (warn "Failed to set up for remote connection:"))
       (setq *socket* socket))))
 
 #+Lucid
 (defun setup-connection-waiting-process (handler-function)
   (setq *connection-waiting-process*
-    (lcl::make-process
-     :function #'handle-connection :args (list handler-function)
-     :wait-function (let ((socket *socket*)
-                  (process lcl::*current-process*))
-              #'(lambda ()
-                  ;; can't use global variables in the wait function.
-                  (let ((port (select-and-accept socket 100)))
-                (if (not (zerop port))
-                    (progn
-                      (setf (lcl::symbol-process-value
-                         '*connection-port* process) port)
-                      t)
-                    nil)))))))
+        (lcl::make-process
+         :function #'handle-connection :args (list handler-function)
+         :wait-function (let ((socket *socket*)
+                              (process lcl::*current-process*))
+                          #'(lambda ()
+                                    ;; can't use global variables in the wait function.
+                                    (let ((port (select-and-accept socket 100)))
+                                      (if (not (zerop port))
+                                        (progn
+                                         (setf (lcl::symbol-process-value
+                                                '*connection-port* process) port)
+                                         t)
+                                        nil)))))))
 
 ;;; the server process calls this on the stream when the connection is made.
 (defun handle-connection (handler-function)
   (cond ((minusp *connection-port*)
-     (when (eq boxer::*boxer-send-server-status* :interrupt)
-       (close-server)
-       (format t "<error: connection lost -- killing process>~%")))
+         (when (eq boxer::*boxer-send-server-status* :interrupt)
+           (close-server)
+           (format t "<error: connection lost -- killing process>~%")))
     (t
      (let ((stream nil))
        (unwind-protect
         (progn (setq stream
-                 #+Lucid (lcl::make-lisp-stream :input-handle  *connection-port*
-                                :output-handle  *connection-port*
-                                :element-type '(unsigned-byte 16))
-                 #-Lucid (warn "~S undefined in this Lisp" 'make-lisp-stream))
+                     #+Lucid (lcl::make-lisp-stream :input-handle  *connection-port*
+                                                    :output-handle  *connection-port*
+                                                    :element-type '(unsigned-byte 16))
+                     #-Lucid (warn "~S undefined in this Lisp" 'make-lisp-stream))
                (funcall handler-function stream))
-         (close stream)
-         (setq *connection-port* nil))))))
+        (close stream)
+        (setq *connection-port* nil))))))
 
 (defun close-server ()
   (when (and *socket* (plusp *socket*))
@@ -202,9 +202,9 @@ Modification History (most recent at top)
   (when (not (null *polling-handler*))
     (let ((port (select-and-accept *socket*  *net-connection-toplevel-polling-time*)))
       (when (not (zerop port))
-    (setq *socket* nil)
-    (setq *connection-port* port)
-    (handle-connection *polling-handler*)))))
+        (setq *socket* nil)
+        (setq *connection-port* port)
+        (handle-connection *polling-handler*)))))
 
 
 (defun enable-polling (function)
@@ -218,14 +218,14 @@ Modification History (most recent at top)
 ;;; send
 (defmacro with-open-port-stream ((stream-var hostname) &body body)
   `(with-open-stream
-       (,stream-var (let ((fd (socket-open-and-connect ,hostname)))
-              (when (minusp fd)
-            (boxer-eval::primitive-signal-error
-             "Couldn't connect to host:" ,hostname))
-              #+Lucid (lcl:make-lisp-stream :input-handle fd
-                        :output-handle fd
-                        :element-type '(unsigned-byte 16))
-              #-Lucid (warn "~S undefined for this Lisp" 'MAKE-LISP-STREAM)))
+     (,stream-var (let ((fd (socket-open-and-connect ,hostname)))
+                    (when (minusp fd)
+                      (boxer-eval::primitive-signal-error
+                       "Couldn't connect to host:" ,hostname))
+                    #+Lucid (lcl:make-lisp-stream :input-handle fd
+                                                  :output-handle fd
+                                                  :element-type '(unsigned-byte 16))
+                    #-Lucid (warn "~S undefined for this Lisp" 'MAKE-LISP-STREAM)))
      .,BODY))
 
 
