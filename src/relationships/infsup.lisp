7d3159a9973456036a27a8a94013d3d054c39a2e
Steven Githens <steve@githens.org>
2020-11-02 10:19:40 -0800

    Converting top block comments to semicolons

--- src/relationships/infsup.lisp
+++ src/relationships/infsup.lisp
@@ -1,61 +1,52 @@
 ;; -*- Mode:LISP; Package:(BOXER LISP 1000); Base:10.-*-
-#|
-
-
- $Header: infsup.lisp,v 1.0 90/01/24 22:13:08 boxer Exp $
-
- $Log:	infsup.lisp,v $
-;;;Revision 1.0  90/01/24  22:13:08  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                         +-Data--+
-                This file is part of the | BOXER | system
-                                         +-------+
-
-
-
-
-  This file contains low-level code which deals with the inferior/superior
-  relations between primitive Boxer objects. These relations include the
-  connection/disconnection of primitive Boxer objects from their superiors
-  and from groups of co-inferiors.
-
-
-
-Modification History (most recent at the top)
-
- 2/11/03 merged current LW and MCL files
- 2/15/01 merged current LW and MCL files
-11/15/00 compact-fds hacks fonted empty row case. Make sure compact-fds is called
-         last in chas-array-delete-cha and chas-array-insert-cha
-10/23/99 char-array-insert-bfd: fixed bug when adding a BFD with a higher cha-no
-         to a chas-array with existing BFD's
- 5/06/99 chas-array-move-chas special case for bfd at 0
- 7/30/98 copy-fd-at is a convenience for copying FD values at a new cha-no
-         change chas-array-move-chas and insert-cha-at-cha-no to use it
- 5/11/98 changed compact-fds to prefer rightward FD's to fix rubout bug
- 5/04/98 more font fixes, chas-array-move-chas, insert-cha-at-cha-no
- 5/03/98 chas-array-slide-fds no longer compacts use compact-fds instead
- 4/30/98 chas-array-insert-bfd didn't hack the case where the new FD goes 1st
- 4/29/98 insert-cha-at-cha-no: lots o' fixes
-         chas-array-insert-fd hacks colors now.  Also, just sets to new font
-         instead of logior'ing
- 4/27/98 Started Logging changes: source = boxer version 2.2.r4 + changes for fonts
-
-
-|#
+;;;;
+;;;;        Boxer
+;;;;        Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;        Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;        used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;        Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;        https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                             +-Data--+
+;;;;                    This file is part of the | BOXER | system
+;;;;                                             +-------+
+;;;;
+;;;;
+;;;;
+;;;;
+;;;;      This file contains low-level code which deals with the inferior/superior
+;;;;      relations between primitive Boxer objects. These relations include the
+;;;;      connection/disconnection of primitive Boxer objects from their superiors
+;;;;      and from groups of co-inferiors.
+;;;;
+;;;;
+;;;;
+;;;;    Modification History (most recent at the top)
+;;;;
+;;;;     2/11/03 merged current LW and MCL files
+;;;;     2/15/01 merged current LW and MCL files
+;;;;    11/15/00 compact-fds hacks fonted empty row case. Make sure compact-fds is called
+;;;;             last in chas-array-delete-cha and chas-array-insert-cha
+;;;;    10/23/99 char-array-insert-bfd: fixed bug when adding a BFD with a higher cha-no
+;;;;             to a chas-array with existing BFD's
+;;;;     5/06/99 chas-array-move-chas special case for bfd at 0
+;;;;     7/30/98 copy-fd-at is a convenience for copying FD values at a new cha-no
+;;;;             change chas-array-move-chas and insert-cha-at-cha-no to use it
+;;;;     5/11/98 changed compact-fds to prefer rightward FD's to fix rubout bug
+;;;;     5/04/98 more font fixes, chas-array-move-chas, insert-cha-at-cha-no
+;;;;     5/03/98 chas-array-slide-fds no longer compacts use compact-fds instead
+;;;;     4/30/98 chas-array-insert-bfd didn't hack the case where the new FD goes 1st
+;;;;     4/29/98 insert-cha-at-cha-no: lots o' fixes
+;;;;             chas-array-insert-fd hacks colors now.  Also, just sets to new font
+;;;;             instead of logior'ing
+;;;;     4/27/98 Started Logging changes: source = boxer version 2.2.r4 + changes for fonts
+;;;;
+;;;;
+;;;;
 
 (in-package :boxer)
 
