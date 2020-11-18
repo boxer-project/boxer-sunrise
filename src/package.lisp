7ab19625960e02e81bb2bce829746c85646f8a00
Steven Githens <steve@githens.org>
2020-11-18 13:11:47 -0800

    Converting package.lisp header comments to semicolons.

--- src/package.lisp
+++ src/package.lisp
@@ -1,31 +1,22 @@
-#|
-
- $Header: boxdef.lisp,v 1.0 90/01/24 22:06:55 boxer Exp $
-
- $Log:	boxdef.lisp,v $
-;;;Revision 1.0  90/01/24  22:06:55  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                         +-Data--+
-                This file is part of the | BOXER | system
-                                         +-------+
-
-
- This file contains the package declarations and other symbol import/exports for Boxer.
-
-|#
+;;;;
+;;;;     Boxer
+;;;;     Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;     Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;     used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;     Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;     https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                          +-Data--+
+;;;;                 This file is part of the | BOXER | system
+;;;;                                          +-------+
+;;;;
+;;;;
+;;;;  This file contains the package declarations and other symbol import/exports for Boxer.
+;;;;
 
 (defpackage :boxer-user
   (:use)
