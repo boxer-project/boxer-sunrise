b28185a561ea79b95df209f6986378488eb6b3b4
Steven Githens <steve@githens.org>
2020-11-18 18:16:27 -0800

    Converting storage.lisp header block to semicolons

--- src/definitions/storage.lisp
+++ src/definitions/storage.lisp
@@ -1,41 +1,30 @@
-;; -*- Mode:LISP;Syntax:Common-Lisp; Package:BOXER ;-*-
-
-#|
-
-
- $Header: storage.lisp,v 1.0 90/01/24 22:17:58 boxer Exp $
-
- $Log:	storage.lisp,v $
-;;;Revision 1.0  90/01/24  22:17:58  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                         +-------+
-                This file is part of the | BOXER | system
-                                         +-------+
-
-  This file contains a storage substrate used by the redisplay (although anyone
-  else can use it as well) to keep track of inferior items
-
-
-
-Modification History (most recent at top)
-
- 2/10/03 merged current PC & mac source
-10/25/98 Additions for Harlequin Lispworks for Windows 4.1
-
-|#
+;;;;  ;; -*- Mode:LISP;Syntax:Common-Lisp; Package:BOXER ;-*-
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                           +-------+
+;;;;                  This file is part of the | BOXER | system
+;;;;                                           +-------+
+;;;;
+;;;;    This file contains a storage substrate used by the redisplay (although anyone
+;;;;    else can use it as well) to keep track of inferior items
+;;;;
+;;;;
+;;;;
+;;;;  Modification History (most recent at top)
+;;;;
+;;;;   2/10/03 merged current PC & mac source
+;;;;  10/25/98 Additions for Harlequin Lispworks for Windows 4.1
+;;;;
 
 (in-package :boxer)
 
