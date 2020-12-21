627b48302279a18c66d363602c975ee0e8677702
Steven Githens <steve@githens.org>
2020-12-21 10:20:57 -0800

    Converting header comments to semicolons in grobjs.lisp

--- src/grfdefs/grobjs.lisp
+++ src/grfdefs/grobjs.lisp
@@ -1,46 +1,36 @@
-;; -*- Mode:LISP; Syntax:Common-Lisp;Package:BOXER;-*-
-#|
-
-
- $Header: grobjs.lisp,v 1.0 90/01/24 22:12:46 boxer Exp $
-
- $Log:	grobjs.lisp,v $
-;;;Revision 1.0  90/01/24  22:12:46  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                       +-Data--+
-              This file is part of the | BOXER | system
-                                       +-------+
-
-
- Graphics Object Definitions
- Coordinate Transformation and Drawing Utilities
- Also mouse-sensitivity code.
-
-
-
-Modification History (most recent at top)
-
- 2/11/13 moved redisplay init to gdispl.lisp to get dependencies correct
- 1/28/08 added private graphics list slot to turtle class
- 3/20/07 add/remove-graphics-object changed to handle graphics-info paradigm
- 3/05/07 *default-turtle-shape* is now PD not XOR
- 2/26/01 array-coordinate-x,y: added float coercion
- 5/06/98 started logging: source = Boxer version 2.3
-
-|#
+;;;; -*- Mode:LISP; Syntax:Common-Lisp;Package:BOXER;-*-
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                         +-Data--+
+;;;;                This file is part of the | BOXER | system
+;;;;                                         +-------+
+;;;;
+;;;;
+;;;;   Graphics Object Definitions
+;;;;   Coordinate Transformation and Drawing Utilities
+;;;;   Also mouse-sensitivity code.
+;;;;
+;;;;
+;;;;
+;;;;  Modification History (most recent at top)
+;;;;
+;;;;   2/11/13 moved redisplay init to gdispl.lisp to get dependencies correct
+;;;;   1/28/08 added private graphics list slot to turtle class
+;;;;   3/20/07 add/remove-graphics-object changed to handle graphics-info paradigm
+;;;;   3/05/07 *default-turtle-shape* is now PD not XOR
+;;;;   2/26/01 array-coordinate-x,y: added float coercion
+;;;;   5/06/98 started logging: source = Boxer version 2.3
+;;;;
 
 (in-package :boxer)
 
