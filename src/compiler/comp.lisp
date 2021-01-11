4f9f2713681370323a828c678beb31a8f8ba59a2
Steven Githens <steve@githens.org>
2021-01-11 06:47:50 -0800

    format Converting Header comments to semicolons

--- src/compiler/comp.lisp
+++ src/compiler/comp.lisp
@@ -1,36 +1,28 @@
-;;-*- Mode:Lisp; Syntax: Common-Lisp; package: Boxer; -*-
-
-#|
-
- $Header$
-
- $Log$
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                         +-Data--+
-                This file is part of the | BOXER | system
-                                         +-------+
-
-
-            This file contains BUILD and its supporting functions
-
-
-Modification History (most recent at top)
-
- 2/15/03 merged current LW and MCL files, no diffs, copyright updated
-
-
-|#
+;;;;  ;;-*- Mode:Lisp; Syntax: Common-Lisp; package: Boxer; -*-
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                           +-Data--+
+;;;;                  This file is part of the | BOXER | system
+;;;;                                           +-------+
+;;;;
+;;;;
+;;;;              This file contains BUILD and its supporting functions
+;;;;
+;;;;
+;;;;  Modification History (most recent at top)
+;;;;
+;;;;   2/15/03 merged current LW and MCL files, no diffs, copyright updated
+;;;;
 
 (in-package :boxer)
 
