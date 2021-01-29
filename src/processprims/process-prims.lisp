7a663e2ddd59d2703ddf0a2e011f76016afae5be
Steven Githens <steve@githens.org>
2021-01-29 15:11:50 -0800

    format Converting process-prims header to semicolons

--- src/processprims/process-prims.lisp
+++ src/processprims/process-prims.lisp
@@ -1,34 +1,27 @@
-;;;-*-LISP-*-
-#|
-
- $Header: process-prims.lisp,v 1.0 90/01/24 22:15:51 boxer Exp $
-
- $Log:	process-prims.lisp,v $
-;;;Revision 1.0  90/01/24  22:15:51  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                      +-------+
-             This file is part of the | Boxer | System
-                                      +-Data--+
-
-
-Modification History (most recent at top)
-
- 4/21/03 merged current LW and MCL files, no diffs, updated copyright
-
-|#
+;;;;-*-LISP-*-
+;;;;
+;;;;   $Header: process-prims.lisp,v 1.0 90/01/24 22:15:51 boxer Exp $
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                        +-------+
+;;;;               This file is part of the | Boxer | System
+;;;;                                        +-Data--+
+;;;;
+;;;;
+;;;;  Modification History (most recent at top)
+;;;;
+;;;;   4/21/03 merged current LW and MCL files, no diffs, updated copyright
+;;;;
 
 (in-package :boxer-eval)
 
