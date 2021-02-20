8467cc5dc0153efd50f481e3addfe283052faa6e
Steven Githens <steve@githens.org>
2021-02-20 10:00:03 -0800

    boxer-sunrise-11 Updating start script to take in to account windows drive letters for paths, as well as the slightly difference directory sturcture, and load 64ofasl files for windows and 64xfasl files for MacOS

--- src/start-boxer.lisp
+++ src/start-boxer.lisp
@@ -20,17 +20,28 @@
 |#
 (in-package :boxer)
 
+(defun base-install-folder ()
+  "Returns the base installed folder of boxer, which can be used to create deeper paths
+   to the folders for fonts, libraries, and such from wherever boxer is installed or
+   being run from the file system.
+
+   This differs slightly between MacOS and Windows because of the folder the executable is
+   placed in."
+   #+mac (butlast (pathname-directory (lw:lisp-image-name)))
+   #+win32 (pathname-directory (lw:lisp-image-name)))
+
 (defun start-boxer ()
     ;; Here we are adding the Resources/libs directory of the application bundle
     ;; which will libfreetype.6.dylib, as well as any other *.dylib and *.dll files.
     (pushnew
-        (make-pathname :directory
-            (append (butlast (pathname-directory (lw:lisp-image-name)))
-            '("Resources" "libs")))
+        (make-pathname :host (pathname-host (lw:lisp-image-name))
+                       :directory(append (base-install-folder) '("Resources" "libs")))
         cffi:*foreign-library-directories* :test #'equal)
 
     ;; This adds the directory where we are keeping the compiled version of freetype2,
     ;; such that it can be included eventually using asdf:load-system.
+
+    ;; sgithens, I don't think this is necessary at the moment since we're manually loading things...
     (setf asdf:*central-registry*
                (list* '*default-pathname-defaults*
                       (make-pathname :directory
@@ -51,43 +62,47 @@
     ;;  other possible fixes. Hopefully this will be removed in the near future and the
     ;;  asdf:load-system call used instead.
     ;; (asdf:load-system :cl-freetype2)
-    (let ((freetype-deps-dir (make-pathname :directory
-            (append (butlast (pathname-directory (lw:lisp-image-name)))
-            '("Resources" "cl-deps" "cl-freetype2" "src")))))
-        (load (merge-pathnames "package.64xfasl" freetype-deps-dir))
+    (let ((freetype-deps-dir (make-pathname
+                               :host (pathname-host (lw:lisp-image-name))
+                               :directory (append (base-install-folder) '("Resources" "cl-deps" "cl-freetype2" "src"))))
+          (freetype-source-parts '("package"
+                                   "ffi/grovel/grovel-freetype2"
+                                   "ffi/cffi-cwrap"
+                                   "ffi/cffi-defs"
+                                   "ffi/ft2-lib"
+                                   "ffi/ft2-init"
+                                   "ffi/ft2-basic-types"
+                                   "ffi/ft2-face"
+                                   "ffi/ft2-glyph"
+                                   "ffi/ft2-size"
+                                   "ffi/ft2-outline"
+                                   "ffi/ft2-bitmap"
+                                   "init"
+                                   "face"
+                                   "bitmap"
+                                   "glyph"
+                                   "render"
+                                   "outline"
+                                   "toy")))
+      (dolist (source-part freetype-source-parts)
+        ;; Currently the file extensions for 64-bit x86 compiled lisp files on MacOS and Windows
+        ;; are .64xfasl and 64ofasl respectively
+        #+mac (load (merge-pathnames (concatenate 'string source-part ".64xfasl") freetype-deps-dir))
+        #+win32 (load (merge-pathnames (concatenate 'string source-part ".64ofasl") freetype-deps-dir))))
 
-        (load (merge-pathnames "ffi/grovel/grovel-freetype2.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/cffi-cwrap.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/cffi-defs.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-lib.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-init.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-basic-types.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-face.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-glyph.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-size.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-outline.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "ffi/ft2-bitmap.64xfasl" freetype-deps-dir))
-
-        (load (merge-pathnames "init.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "face.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "bitmap.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "glyph.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "render.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "outline.64xfasl" freetype-deps-dir))
-        (load (merge-pathnames "toy.64xfasl" freetype-deps-dir)))
 
     ;; Adding the fonts directory based on whereever this MacOS application happens to
     ;; be running from.
-    (setf boxer-window::*capogi-font-directory* (make-pathname :directory
-                                                    (append (butlast (pathname-directory (lw:lisp-image-name)))
-                                                    '("Resources" "Fonts"))) )
+    (setf boxer-window::*capogi-font-directory* (make-pathname
+                                                  :host (pathname-host (lw:lisp-image-name))
+                                                  :directory (append (base-install-folder) '("Resources" "Fonts"))))
 
     ;; See the above comments on freetype, but we are manually loading our code for it
     ;; here, because the freetype package is not yet declared when we build our system.
-    (load (make-pathname :directory
-                        (append (butlast (pathname-directory (lw:lisp-image-name)))
-                        '("Resources" "cl-deps"))
-                   :name "freetype-fonts" :type "lisp"))
+    (load (make-pathname
+            :host (pathname-host (lw:lisp-image-name))
+            :directory (append (base-install-folder) '("Resources" "cl-deps"))
+            :name "freetype-fonts" :type "lisp"))
 
     (boxer-window::window-system-specific-make-boxer)
     (boxer-window::window-system-specific-start-boxer)
