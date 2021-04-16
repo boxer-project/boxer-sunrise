bdb4c371aa472dabf645f5bd78f9c7832d9559d6
Steven Githens <steve@githens.org>
2021-04-16 12:56:56 -0700

    format Fixing up varmacs.lisp headers

--- src/evalvars/varmacs.lisp
+++ src/evalvars/varmacs.lisp
@@ -1,36 +1,24 @@
-;;;-*-LISP-*-
-#|
-
-
- $Header: varmacs.lisp,v 1.0 90/01/24 22:18:58 boxer Exp $
-
- $Log:	varmacs.lisp,v $
-;;;Revision 1.0  90/01/24  22:18:58  boxer
-;;;Initial revision
-;;;
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                      +-------+
-             This file is part of the | Boxer | System
-                                      +-Data--+
-
-
-
-Modification History (most recent at the top)
-
- 2/11/02 merged current LW and MCL files
-
-|#
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                        +-------+
+;;;;               This file is part of the | Boxer | System
+;;;;                                        +-Data--+
+;;;;
+;;;;
+;;;;  Modification History (most recent at the top)
+;;;;
+;;;;   2/11/02 merged current LW and MCL files
+;;;;
 
 (in-package :boxer-eval)
 
