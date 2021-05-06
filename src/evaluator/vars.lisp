6a37f5e2dc7a3d552353319463049d5b67589b73
Steven Githens <steve@githens.org>
2021-05-06 15:58:47 -0700

    format semicolon formatting header

--- src/evaluator/vars.lisp
+++ src/evaluator/vars.lisp
@@ -1,42 +1,29 @@
-;;; -*- Syntax: Common-lisp; Base: 10; Package: EVAL -*-
-;;;
-;;; $Header: vars.lisp,v 1.0 90/01/24 22:19:04 boxer Exp $
-;;;
-;;; $Log:	vars.lisp,v $
-;;;Revision 1.0  90/01/24  22:19:04  boxer
-;;;Initial revision
-;;;
-#|
-
-    Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
-
-    Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
-    used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
-
-    Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
-
-    https://opensource.org/licenses/BSD-3-Clause
-
-
-                                      +-------+
-             This file is part of the | Boxer | System
-                                      +-Data--+
-
-
-Modification History (most recent at the top)
-
- 1/29/08 added boxer::%private-graphics-list
- 3/05/07 *periodic-eval-action* set to boxer::repaint-in-eval
-10/21/03 *periodic-eval-action/times* carbon values set for 700 MHz G4 imac
- 2/11/03 merged current LW and MCL files
-12/16/01 added *error-handler-list* and *error-signalled?*
-12/16/01 started logging changes: source = boxer version 2.5
-
-|#
-
-
-;;;
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                        +-------+
+;;;;               This file is part of the | Boxer | System
+;;;;                                        +-Data--+
+;;;;
+;;;;
+;;;;  Modification History (most recent at the top)
+;;;;
+;;;;   1/29/08 added boxer::%private-graphics-list
+;;;;   3/05/07 *periodic-eval-action* set to boxer::repaint-in-eval
+;;;;  10/21/03 *periodic-eval-action/times* carbon values set for 700 MHz G4 imac
+;;;;   2/11/03 merged current LW and MCL files
+;;;;  12/16/01 added *error-handler-list* and *error-signalled?*
+;;;;  12/16/01 started logging changes: source = boxer version 2.5
+;;;;
 
 (in-package :boxer-eval)
 
