fc1c927b7cc550325045f77b479b64be0cfff595
Steven Githens <steve@githens.org>
2021-11-22 18:21:49 -0800

    boxer-sunrise-47 Temporarily removing new toolbars while debugging macOS Monteray

--- src/boxwin/lw-capi/boxwin-opengl.lisp
+++ src/boxwin/lw-capi/boxwin-opengl.lisp
@@ -447,7 +447,7 @@
               :min-width nil :max-width :screen-width
               :visible-min-height *boxer-status-pane-height*
               :visible-max-height *boxer-status-pane-height*)
-   (status-bar-pane capi:title-pane :text "")
+  ;;  (status-bar-pane capi:title-pane :text "")
    (boxer-pane opengl::opengl-pane
                :configuration #-linux '(:rgba t :depth nil :double-buffered t :aux 1)
                               #+linux '(:rgba t :depth nil :double-buffered t ) ;:aux 1) TODO This aux option crashes LW on linux
@@ -677,7 +677,8 @@
                ))
   (:layouts
    (boxer-layout capi:column-layout
-                 '(text-toolbar name-pane boxer-pane status-bar-pane)
+                ;;  '(text-toolbar name-pane boxer-pane status-bar-pane)
+                '(name-pane boxer-pane)
                  :columns 1 :rows 2 :y-gap 1 :x-uniform-size-p t))
   ;; menu item actions are defined in lw-menu.lisp
   (:menus
@@ -946,7 +947,9 @@
 
 (defvar *display-bootstrapping-no-boxes-yet* t)
 
-(defun update-visible-editor-panes ()
+(defun update-visible-editor-panes ())
+
+(defun update-visible-editor-panes-skip ()
   "This function takes into account the current user preferences for dispalying the toolbar and
   status bar, and re-lays out the main boxer-frame layout taking those in to account"
   (let ((layout (slot-value *boxer-frame* 'capi:layout))
