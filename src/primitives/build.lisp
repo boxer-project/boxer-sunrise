93dc8ca848a67f5fd409daf9b2859136d3b81b98
Steven Githens <steve@githens.org>
2022-02-12 10:31:45 -0800

    copyright Updating copyright year to 2022

--- src/primitives/build.lisp
+++ src/primitives/build.lisp
@@ -7,7 +7,7 @@
  $Log$
 
     Boxer
-    Copyright 1985-2020 Andrea A. diSessa and the Estate of Edward H. Lay
+    Copyright 1985-2022 Andrea A. diSessa and the Estate of Edward H. Lay
 
     Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
     used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
