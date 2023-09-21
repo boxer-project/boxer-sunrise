61a7f906c42e2fce3ed1d81b47a530d510145dbe
Steven Githens <steve@githens.org>
2023-09-21 15:42:33 -0700

    bugs-191 Removing the Repaint item from the Help menu for now

--- src/boxwin/lw-capi/boxwin-opengl.lisp
+++ src/boxwin/lw-capi/boxwin-opengl.lisp
@@ -718,7 +718,10 @@
                                                      :callback 'menu-name-spelling-help)
                                     ("Key/Mouse" ;:accelerator "Alt-?"
                                      :callback 'menu-key-mouse-help)))
-                       (:component (("Repaint" :accelerator #\r :callback 'menu-redisplay)))))
+                       ;; bugs-191 Removing this from the menu for now, not sure if it really
+                       ;; ever has a place on there again
+                       ;;  (:component (("Repaint" :accelerator #\r :callback 'menu-redisplay)))
+                       ))
     ;; submenus
     (display-props-sub-menu "Display Properties..."
                             (("Zoom Control" :callback 'menu-zoom-toggle
