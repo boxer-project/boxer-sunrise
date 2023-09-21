40ed44a0c05b868eae8bb802e098d7dc5fb3e76c
Steven Githens <steve@githens.org>
2023-09-21 10:16:17 -0700

    sunrise-69 Cleaning up graphics commands by adding defclass graphics-command and defmacro defdraw-graphics-command

--- src/grfdefs/boxer-graphics-commands.lisp
+++ src/grfdefs/boxer-graphics-commands.lisp
@@ -18,50 +18,13 @@
 ;;;; 62   BOXER-FILLED-CIRCLE                          (X Y RADIUS)
 ;;;; 63   BOXER-CIRCLE                                 (X Y RADIUS)
 
-(defun apply-gdispl-com (fun vec)
-  (apply fun (cdr (coerce vec 'list))))
+(defun apply-gdispl-com (gc vec)
+  (apply #'draw-gc (cons gc (list (cdr (coerce vec 'list))))))
 
 (defun process-graphics-command (command)
-  (let ((com (aref command 0)))
-    (cond ((eq com 32)
-           (apply-gdispl-com #'draw-boxer-change-alu command))
-          ((eq com 33)
-           (apply-gdispl-com #'draw-boxer-change-pen-width command))
-          ((eq com 34)
-           (apply-gdispl-com #'draw-boxer-change-graphics-font command))
-          ((eq com 35)
-           (apply-gdispl-com #'draw-boxer-line-segment command))
-          ((eq com 36)
-           (apply-gdispl-com #'draw-boxer-change-graphics-color command))
-          ((eq com 37)
-           (apply-gdispl-com #'draw-boxer-transform-matrix command)
-          )
-          ((eq com 39)
-           (apply-gdispl-com #'draw-boxer-centered-string command))
-          ((eq com 40)
-           (apply-gdispl-com #'draw-boxer-left-string command))
-          ((eq com 41)
-           (apply-gdispl-com #'draw-boxer-right-string command))
-          ((eq com 42)
-           (apply-gdispl-com #'draw-boxer-centered-rectangle command))
-          ((eq com 43)
-           (apply-gdispl-com #'draw-boxer-dot command))
-          ((eq com 47)
-           (apply-gdispl-com #'draw-boxer-centered-bitmap command))
-          ((eq com 58)
-           (apply-gdispl-com #'draw-boxer-wedge command))
-          ((eq com 59)
-           (apply-gdispl-com #'draw-boxer-arc command))
-          ((eq com 60)
-           (apply-gdispl-com #'draw-boxer-filled-ellipse command))
-          ((eq com 61)
-           (apply-gdispl-com #'draw-boxer-ellipse command))
-          ((eq com 62)
-           (apply-gdispl-com #'draw-boxer-filled-circle command))
-          ((eq com 63)
-           (apply-gdispl-com #'draw-boxer-circle command))
-          (t
-          (break "Unhandled boxer graphics playback: ~A" command)))))
+  (let* ((com (aref command 0))
+         (handler (gethash com *graphics-commands*)))
+    (apply-gdispl-com handler command)))
 
 (defun boxer-playback-graphics-list (gl &key (start 0) (graphics-canvas nil) (translate? nil))
   "In progress work to move all the boxer graphics to turtle coordinates. Will get trued
@@ -103,21 +66,43 @@
         (setf (boxgl-device-model-matrix bw::*boxgl-device*) prev-model)
         (update-matrices-ubo bw::*boxgl-device*)))))
 
+(defclass graphics-command () ())
+
+(defmacro defdraw-graphics-command ((gc-class &rest method-args) &body body)
+  `(defmethod draw-gc ((self ,gc-class) com)
+     (let ,(loop for i
+                 from 0
+                 to (1- (length method-args))
+                 collect (list (nth i method-args) (list 'nth i 'com)))
+       . ,body)))
+
 ;; 32   BOXER-CHANGE-ALU                             (NEW-ALU)
-(defun draw-boxer-change-alu (new-alu)
+(defclass boxer-change-alu (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-change-alu new-alu)
   (setq *graphics-state-current-alu* new-alu))
 
 ;; 33   BOXER-CHANGE-PEN-WIDTH              (NEW-WIDTH)
+(defclass boxer-change-pen-width (graphics-command)
+  ())
 
-(defun draw-boxer-change-pen-width (new-width)
+(defdraw-graphics-command (boxer-change-pen-width new-width)
   (%set-pen-size new-width))
 
 ;; 34   BOXER-CHANGE-GRAPHICS-FONT          (NEW-FONT-NO)
-(defun draw-boxer-change-graphics-font (new-font-no)
+(defclass boxer-change-graphics-font (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-change-graphics-font new-font-no)
   (setq *graphics-state-current-font-no* new-font-no))
 
+
 ;; 35   BOXER-LINE-SEGMENT                           (X0 Y0 X1 Y1)
-(defun draw-boxer-line-segment (x0 y0 x1 y1)
+(defclass boxer-line-segment (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-line-segment x0 y0 x1 y1)
   "Takes a boxer command starting with 35:
     ex #(35 -0.5 -0.5 0.0 0.5)"
   ;; The y-axis needs to be flipped
@@ -125,18 +110,26 @@
                      *graphics-state-current-alu*))
 
 ;; 36   BOXER-CHANGE-GRAPHICS-COLOR                  (NEW-COLOR)
-(defun draw-boxer-change-graphics-color (new-color)
+(defclass boxer-change-graphics-color (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-change-graphics-color new-color)
   (setf *graphics-state-current-pen-color* new-color)
   (%set-pen-color new-color))
 
 ;; 37   BOXER-TRANSFORM-MATRIX                       (. . .)
+(defclass boxer-transform-matrix (graphics-command)
+  ())
 
-(defun draw-boxer-transform-matrix (matrix)
+(defdraw-graphics-command (boxer-transform-matrix matrix)
   (setf (boxgl-device-model-matrix bw::*boxgl-device*) matrix)
   (update-matrices-ubo bw::*boxgl-device*))
 
 ;; 39   BOXER-CENTERED-STRING             (X Y STRING)
-(defun draw-boxer-centered-string (x y text)
+(defclass boxer-centered-string (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-centered-string x y text)
   (let ((y (- y)))
     (do* ((height (1+ (string-hei *graphics-state-current-font-no*)))
           (s text (subseq s (let ((p (position #\newline s)))
@@ -152,7 +145,10 @@
       (draw-string *graphics-state-current-font-no* trimmed-string wx wy))))
 
 ;;;; 40   BOXER-LEFT-STRING          (X Y STRING)
-(defun draw-boxer-left-string (x y text)
+(defclass boxer-left-string (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-left-string x y text)
   (let ((y (- y)))
     (loop
       (draw-string *graphics-state-current-font-no*
@@ -164,8 +160,12 @@
                             (if (null p) 0 (1+ p))))
               y (+ y 1 (string-hei *graphics-state-current-font-no*)))))))
 
+
 ;;;; 41   BOXER-RIGHT-STRING         (X Y STRING)
-(defun draw-boxer-right-string (x y text)
+(defclass boxer-right-string (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-right-string x y text)
   (let ((y (- y))
         (width 0)
         (trimmed-string ""))
@@ -180,53 +180,108 @@
                                   (if (null p) 0 (1+ p))))
               y (+ y (1+ (string-hei *graphics-state-current-font-no*))))))))
 
+
 ;; 42   BOXER-CENTERED-RECTANGLE       (X Y WIDTH HEIGHT)
-(defun draw-boxer-centered-rectangle (x y w h)
+(defclass boxer-centered-rectangle (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-centered-rectangle x y w h)
     (draw-rectangle w h
                    (- x (/ w 2))
                    ;; The y axis is flipped
                    (- (+ y (/ h 2)))))
 
+
 ;; 43   BOXER-DOT                      (X Y)
-(defun draw-boxer-dot (x y)
+(defclass boxer-dot (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-dot x y)
     (draw-rectangle *graphics-state-current-pen-width* *graphics-state-current-pen-width*
                  (- x (/ *graphics-state-current-pen-width* 2))
                  (- (+ y (/ *graphics-state-current-pen-width* 2)))))
 
+
 ;; 47   BOXER-CENTERED-BITMAP          (BITMAP X Y WIDTH HEIGHT)
-(defun draw-boxer-centered-bitmap (bitmap x y wid hei)
+(defclass boxer-centered-bitmap (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-centered-bitmap bitmap x y wid hei)
     (bitblt-to-screen wid hei bitmap 0 0
                       (- x (floor wid 2))
                       (- (+ y (floor hei 2)))))
 
+
 ;; 58   BOXER-WEDGE                    (X Y RADIUS START-ANGLE SWEEP-ANGLE)
-(defun draw-boxer-wedge (x y radius start-angle sweep-angle)
+(defclass boxer-wedge (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-wedge x y radius start-angle sweep-angle)
   (let ((y (- y)))
     (when (plusp radius)
       (%draw-c-arc x y radius start-angle sweep-angle t))))
 
 ;; 59   BOXER-ARC                      (X Y RADIUS START-ANGLE SWEEP-ANGLE)
-(defun draw-boxer-arc (x y radius start-angle sweep-angle)
+(defclass boxer-arc (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-arc x y radius start-angle sweep-angle)
   (let ((y (- y)))
     (when (plusp radius)
       (%draw-c-arc x y radius start-angle sweep-angle nil))))
 
+
 ;; 60   BOXER-FILLED-ELLIPSE        (X Y WIDTH HEIGHT)
-(defun draw-boxer-filled-ellipse (x y w h)
+(defclass boxer-filled-ellipse (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-filled-ellipse x y w h)
   (let ((y (- y)))
     (draw-ellipse x y w h t)))
 
+
 ;; 61   BOXER-ELLIPSE               (X Y WIDTH HEIGHT)
-(defun draw-boxer-ellipse (x y w h)
+(defclass boxer-ellipse (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-ellipse x y w h)
   (let ((y (- y)))
     (draw-ellipse x y w h nil)))
 
 ;; 62   BOXER-FILLED-CIRCLE        (X Y RADIUS)
-(defun draw-boxer-filled-circle (x y radius)
+(defclass boxer-filled-circle (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-filled-circle x y radius)
   (let ((y (- y)))
     (draw-circle x y radius t)))
 
+
 ;; 63   BOXER-CIRCLE               (X Y RADIUS)
-(defun draw-boxer-circle (x y radius)
+(defclass boxer-circle (graphics-command)
+  ())
+
+(defdraw-graphics-command (boxer-circle x y radius)
   (let ((y (- y)))
     (draw-circle x y radius nil)))
+
+
+(defparameter *graphics-commands*
+  (serapeum:dict 32 (make-instance 'boxer-change-alu)
+                 33 (make-instance 'boxer-change-pen-width)
+                 34 (make-instance 'boxer-change-graphics-font)
+                 35 (make-instance 'boxer-line-segment)
+                 36 (make-instance 'boxer-change-graphics-color)
+                 37 (make-instance 'boxer-transform-matrix)
+                 39 (make-instance 'boxer-centered-string)
+                 40 (make-instance 'boxer-left-string)
+                 41 (make-instance 'boxer-right-string)
+                 42 (make-instance 'boxer-centered-rectangle)
+                 43 (make-instance 'boxer-dot)
+                 47 (make-instance 'boxer-centered-bitmap)
+                 58 (make-instance 'boxer-wedge)
+                 59 (make-instance 'boxer-arc)
+                 60 (make-instance 'boxer-filled-ellipse)
+                 61 (make-instance 'boxer-ellipse)
+                 62 (make-instance 'boxer-filled-circle)
+                 63 (make-instance 'boxer-circle)))
