1f5954b00dd68c312f9002063b712b514e12a294
Steven Githens <steve@githens.org>
2023-12-05 19:35:09 -0800

    sunrise-11 WIP PR cleanup

--- src/boxwin/lw-capi/boxwin-opengl.lisp
+++ src/boxwin/lw-capi/boxwin-opengl.lisp
@@ -345,8 +345,7 @@
                               #+win32 '(:rgba t :depth nil :double-buffered t)
                               ;#-linux '(:rgba t :depth nil :double-buffered t :aux 1)
                               ;#+linux '(:rgba t :depth nil :double-buffered t ) ;:aux 1) TODO This aux option crashes LW on linux
-               :input-model '(
-                ((:button-1 :press) boxer-click-1-handler)
+               :input-model '(((:button-1 :press) boxer-click-1-handler)
                               ((:button-2 :press) boxer-click-2-handler)
                               ((:button-3 :press) boxer-click-3-handler)
 
