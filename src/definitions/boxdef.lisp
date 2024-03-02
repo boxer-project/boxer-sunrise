7f157415bfbcac8bee3df5fd4cd9515290c8819d
Steven Githens <steve@githens.org>
2024-03-02 08:13:47 -0800

    changelog version Bumping version 3.4.17

--- src/definitions/boxdef.lisp
+++ src/definitions/boxdef.lisp
@@ -50,7 +50,7 @@ Modification History (most recent at top)
 
 (in-package :boxer)
 
-(defvar *boxer-version* "3.4.16 2024-02-26")
+(defvar *boxer-version* "3.4.17 2024-03-02")
 
 (defun boxer-component-version ()
   "Returns the current semver version of boxer based on it's asdf configuration."
