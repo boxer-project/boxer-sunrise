69d1fdfa6462c9ad608e58f46842c25a7986a6af
Steven Githens <steve@githens.org>
2024-03-15 17:39:27 -0700

    debug Adding an outline tree view pane of screen-objs for development.

--- src/boxwin/lw-capi/outline-tree.lisp
+++ src/boxwin/lw-capi/outline-tree.lisp
@@ -18,6 +18,9 @@
 ;;;;
 (in-package :boxer-window)
 
+;;;
+;;; Tree View For Box Data
+;;;
 
 (defun outline-children-function (x)
   (let ((togo '()))
@@ -29,8 +32,7 @@
           ((boxer::row? x)
            (boxer::do-row-chas ((cha x))
              (when (box? cha)
-               (setf togo (cons cha togo))))
-          )
+               (setf togo (cons cha togo)))))
           (t nil))
     (reverse togo)))
 
@@ -38,8 +40,7 @@
   (cond ((box? x)
          (box::name x))
         (t
-         (format nil "~A" x)))
-)
+         (format nil "~A" x))))
 
 (capi:define-interface boxer-outline-tree ()
   ()
@@ -47,8 +48,6 @@
    (tree capi:tree-view
          :roots (list boxer::*initial-box*)
          :children-function #'outline-children-function
-        ;  :image-lists (list :normal *my-image-list*)
-        ;  :image-function #'(lambda (x) (mod (1- x) 4))
          :visible-min-width 200
          :visible-min-height 200
          :retain-expanded-nodes t
@@ -73,3 +72,64 @@
 
 (defun show-outline-tree ()
   (capi:display (make-instance 'boxer-outline-tree :title "Outline")))
+
+;;;
+;;; Tree view for Screen Box Data
+;;;
+
+(defun screen-obj-data-nodes  (item)
+  "Common data for the screen-objs to show in the tree."
+  (with-slots (boxer::x-offset boxer::y-offset) item
+    (list
+      (format nil "x-offset: ~A" boxer::x-offset)
+      (format nil "y-offset: ~A" boxer::y-offset))))
+
+(defmethod screen-obj-children ((self boxer::screen-box))
+  (with-slots (boxer::screen-rows) self
+    (let ((togo (screen-obj-data-nodes self)))
+      (box::do-vector-contents (inf-screen-obj boxer::screen-rows :index-var-name row-no)
+        (setf togo (cons inf-screen-obj togo)))
+      (reverse togo))))
+
+(defmethod screen-obj-children ((self boxer::screen-row))
+  (with-slots (boxer::screen-chas) self
+    (let ((togo (screen-obj-data-nodes self)))
+      ;; For now we'll skip plain characters and just add any boxes in the rows
+      (box::do-vector-contents (inf-screen-obj boxer::screen-chas :index-var-name row-no)
+        (when (not (boxer::screen-cha? inf-screen-obj))
+          (setf togo (cons inf-screen-obj togo))))
+      (reverse togo))))
+
+(defmethod screen-obj-children (self)
+  '())
+
+(capi:define-interface screen-obj-tree ()
+  ()
+  (:panes
+   (tree capi:tree-view
+         :roots (list boxer::*outermost-screen-box*)
+         :children-function #'screen-obj-children
+         :visible-min-width 200
+         :visible-min-height 200
+         :retain-expanded-nodes t
+        ;;  :print-function #'outline-print-function
+         :selection-callback #'(lambda (item self)
+                                 (declare (ignore self))
+                                 (format t "~&Select item ~S~%" item))
+         :action-callback #'(lambda (item self)
+                              (declare (ignore self))
+                              (format t "~&Activate item ~S~%" item)
+                              (when (box? item)
+                                (boxer::set-outermost-box item)))
+         :delete-item-callback #'(lambda (self item)
+                                   (declare (ignore self))
+                                   (format t "~&Delete item ~S~%" item))))
+  (:layouts
+   (default-layout
+    capi:simple-layout
+    '(tree)))
+  (:default-initargs
+   :title "Screen Obj Tree"))
+
+(defun show-screen-obj-tree ()
+  (capi:display (make-instance 'screen-obj-tree :title "Screen Objs")))
