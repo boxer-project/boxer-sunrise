d55ba5e3d8afafb76a475f73b4817b052254f1e0
Steven Githens <steve@githens.org>
2024-05-21 13:17:20 -0700

    sunrise-11 Changing package on defmethods for port height/width

--- src/boxwin/lw-capi/boxer-lw-opengl-canvas.lisp
+++ src/boxwin/lw-capi/boxer-lw-opengl-canvas.lisp
@@ -26,11 +26,11 @@
    (context :initform nil :initarg :context :accessor context)
    (render-state :initform nil :accessor opengl-pane-render-state)))
 
-(defmethod port-width ((self boxer-lw-opengl-canvas))
+(defmethod boxer::port-width ((self boxer-lw-opengl-canvas))
   "Give the width of the port or widget container the Boxer documents canvas. Calculation will be different
    depending on the GL and Widget toolkit used. Needed for scrollbars, panning, and other types of interactions."
   (capi:simple-pane-visible-width self))
 
-(defmethod port-height ((self boxer-lw-opengl-canvas))
+(defmethod boxer::port-height ((self boxer-lw-opengl-canvas))
   "See port-width for description."
   (capi:simple-pane-visible-height self))
