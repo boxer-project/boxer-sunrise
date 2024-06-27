afb4ef60b9702e8c3f9b0b3827db5aaaf636cde6
Steven Githens <steve@githens.org>
2024-06-26 18:41:12 -0700

    sunrise-78 Creating source part for combined killyankcutcopypasteundoredo functionality.

---
+++ src/editor-high/copy-paste-buffers.lisp
@@ -0,0 +1,22 @@
+;;;;  ;; -*- Mode:LISP;Syntax: Common-Lisp; Package:BOXER;-*-
+;;;;
+;;;;      Boxer
+;;;;      Copyright 1985-2022 Andrea A. diSessa and the Estate of Edward H. Lay
+;;;;
+;;;;      Portions of this code may be copyright 1982-1985 Massachusetts Institute of Technology. Those portions may be
+;;;;      used for any purpose, including commercial ones, providing that notice of MIT copyright is retained.
+;;;;
+;;;;      Licensed under the 3-Clause BSD license. You may not use this file except in compliance with this license.
+;;;;
+;;;;      https://opensource.org/licenses/BSD-3-Clause
+;;;;
+;;;;
+;;;;                                           +-Data--+
+;;;;                  This file is part of the | BOXER | system
+;;;;                                           +-------+
+;;;;
+;;;;
+;;;;      This continues utilities for various types Copy, Cut, Paste, Kill, Yank, Redo,
+;;;;      and Undo support.  We want to continue supporting (for at least a while), emacs
+;;;;      Kill/Yank, OS style Cut/Copy/Paste, and some level of Undo/Redo support.
+;;;;
