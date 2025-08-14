4df7160a8d4d5d0c5d0990758f5d7913152285b7
Steven Githens <steve@githens.org>
2025-08-13 18:24:02 -0700

    cleanup Removing wildy obsolete delivery script. The current one is in the scripts folder.

--- deliver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Because of the issue with not loading cl-freetype2 until application start, we
-# are copying this source part to the area in the application bundle where it can
-# still be available after delivery
-cp src/draw/freetype-fonts.lisp data/boxersunrise.app/Contents/PlugIns
-
-/Applications/LispWorks\ 7.1\ \(64-bit\)/LispWorks\ \(64-bit\).app/Contents/MacOS/lispworks-7-1-0-amd64-darwin -build src/delivery-script.lisp
