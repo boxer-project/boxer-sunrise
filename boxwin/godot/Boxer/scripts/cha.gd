8f00d7c152084f3f5d878195fdac0f6397b50554
Steven Githens <steve@githens.org>
2025-12-17 18:18:46 -0800

    godot Recentering turtles again with anchors. Fixing mouse input from getting swallowed by menu node.

--- boxwin/godot/Boxer/scripts/cha.gd
+++ boxwin/godot/Boxer/scripts/cha.gd
@@ -21,6 +21,6 @@ func _on_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed():
         var boxerref = self.get_parent().boxer_row
         clicked.emit(self)
-        var pos = self.get_parent().chas.find(self)
+        var pos = get_index()
         print("Cha clicked2: ", self, " parent: ", self.get_parent().boxer_row, " pos: ", pos)
         $/root/Main.handle_mouse_input(0, boxerref, pos + 1, 0, 0, 0)
