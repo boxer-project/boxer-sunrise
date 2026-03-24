bd42af781418b3e1e581817426369f02d748c91d
Steven Githens <steve@githens.org>
2026-03-24 14:56:07 -0700

    godot Updating handle_mouse_input usage

--- boxwin/godot/Boxer/scripts/row.gd
+++ boxwin/godot/Boxer/scripts/row.gd
@@ -53,20 +53,16 @@ func xpos_in_cha(x, cha_node):
 
 func _on_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed():
-        var action_code = 0
-        if event.double_click: action_code = 3
-        # print("Row GUI Input: ", event)
         if get_child_count() == 0:
-            Global.handle_mouse_input(action_code, boxer_row, 0, Global.BoxArea.INSIDE)
+            Global.handle_mouse_input(event, boxer_row, 0, Global.BoxArea.INSIDE)
         elif sentence_end_xpos() < event.position.x:
             # Is this past the last character?
-            Global.handle_mouse_input(action_code, boxer_row, get_child_count(), Global.BoxArea.INSIDE)
+            Global.handle_mouse_input(event, boxer_row, get_child_count(), Global.BoxArea.INSIDE)
         elif get_child(0).position.x > event.position.x:
             # Is this before the first character?
-            Global.handle_mouse_input(action_code, boxer_row, 0, Global.BoxArea.INSIDE)
+            Global.handle_mouse_input(event, boxer_row, 0, Global.BoxArea.INSIDE)
         else:
             #loop through the chas
             for child in get_children():
                 if xpos_in_cha(event.position.x, child):
-                    Global.handle_mouse_input(action_code, boxer_row, child.get_index()+1, Global.BoxArea.INSIDE)
-
+                    Global.handle_mouse_input(event, boxer_row, child.get_index()+1, Global.BoxArea.INSIDE)
